a92a2e5615f7260b235618b04071f12a69774da8
docscodes <docscodes@gmail.com>
2025-08-04 16:09:21 -0400

    add bar chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import AppBarChart from "@/components/AppBarChart";
+
 const Homepage = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"></div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg"></div>
       <div className="bg-primary-foreground p-4 rounded-lg"></div>
       <div className="bg-primary-foreground p-4 rounded-lg"></div>
