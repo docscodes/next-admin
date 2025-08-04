2e91921a767ac6603c53391593874bdb3e41deac
docscodes <docscodes@gmail.com>
2025-08-04 18:40:28 -0400

    add calendar popover to  todo list

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,11 +1,44 @@
-import { Checkbox } from "@/components/ui/checkbox";
-import { ScrollArea } from "@/components/ui/scroll-area";
+"use client";
+
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Calendar } from "./ui/calendar";
 import { Card } from "./ui/card";
+import { Checkbox } from "./ui/checkbox";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { ScrollArea } from "./ui/scroll-area";
 
 const TodoList = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
-      Calendar
+      <h1 className="text-lg font-medium mb-6">Todo List</h1>
+      <Popover
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <PopoverTrigger asChild>
+          <Button className="w-full">
+            <CalendarIcon />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0 w-auto">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={(date) => {
+              setDate(date);
+              setOpen(false);
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+
       {/* LIST */}
       <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
         <div className="flex flex-col gap-4">
@@ -24,6 +57,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -39,6 +73,20 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
+          {/* LIST ITEM */}
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" />
+              <label
+                htmlFor="item1"
+                className="text-sm text-muted-foreground"
+              >
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              </label>
+            </div>
+          </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -63,6 +111,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -75,6 +124,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -87,6 +137,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -99,18 +150,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
-          {/* LIST ITEM */}
-          <Card className="p-4">
-            <div className="flex items-center gap-4">
-              <Checkbox id="item1" />
-              <label
-                htmlFor="item1"
-                className="text-sm text-muted-foreground"
-              >
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              </label>
-            </div>
-          </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -126,6 +166,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -141,6 +182,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -156,6 +198,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
@@ -171,6 +214,7 @@ const TodoList = () => {
               </label>
             </div>
           </Card>
+
           {/* LIST ITEM */}
           <Card className="p-4">
             <div className="flex items-center gap-4">
