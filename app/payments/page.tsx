594b80abe1d31f6d93208448cb3c0e5280cb0047
docscodes <docscodes@gmail.com>
2025-08-05 10:26:45 -0400

    add data table

--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -1,4 +1,5 @@
-import { Payment } from "./columns";
+import { Payment, columns } from "./columns";
+import { DataTable } from "./data-table";
 
 const getData = async (): Promise<Payment[]> => {
   return [
@@ -265,6 +266,10 @@ const PaymentsPage = async () => {
       <div className="mb-8 px-4 py-2 bg-secondary rounded-md">
         <h1 className="font-semibold">All Payments</h1>
       </div>
+      <DataTable
+        columns={columns}
+        data={data}
+      />
     </div>
   );
 };
